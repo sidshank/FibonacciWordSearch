bc35f23358d92ae9b6267b26002c4f268774b557
Siddhartha Shankar <siddharth.shankar@gmail.com>
2018-06-07 10:41:45 -0400

    Finishing touches. Cleaned up the leaking scanner. Added timing information, and better output formatting.

--- src/KlocWorkDriver.java
+++ src/KlocWorkDriver.java
@@ -1,21 +1,39 @@
 import java.util.Scanner;
-
 import com.klocfun.KlocWorkSearch;
-import com.klocfun.FastKlocWorkSearch;
 
 public class KlocWorkDriver {
+
     public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
-        while(true) {
-            System.out.print("Enter the value of N (-1 to exit):");
-            int n = sc.nextInt();
-            if (n == -1) {
-                break;
+        double timeScalingFactor = Math.pow(10, 6);
+
+        try {
+            while(true) {
+                System.out.print("Enter the value of N (-1 to exit): ");
+
+                int n = sc.nextInt();
+                if (n == -1) {
+                    break;
+                }
+
+                System.out.print("Enter the search string: ");
+                String searchStr = sc.next();
+
+                long tStart = System.nanoTime();
+
+                KlocWorkSearch fkws = new KlocWorkSearch(n, searchStr);
+                long occurrences = fkws.computeOccurrences();
+
+                long tEnd = System.nanoTime();
+
+                System.out.format("\n==========================\n");
+                System.out.format("Search completed in %f ms\n", (tEnd - tStart) / timeScalingFactor);
+                System.out.format("The number of occurrences of %s in kw(%d) is : %s\n",
+                    searchStr, n, occurrences);
+                System.out.format("==========================\n\n");
             }
-            System.out.print("Enter the search string: ");
-            String searchStr = sc.next();
-            FastKlocWorkSearch fkws = new FastKlocWorkSearch(n, searchStr);
-            System.out.println(fkws.computeOccurrences()); 
+        } finally {
+            sc.close();
         }
     }
 }
